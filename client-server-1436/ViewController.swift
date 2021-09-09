f088c754c0e92c3b560040a8cf764c1ac584bf41
tempdos <tempdos@gmail.com>
2021-09-09 10:03:13 +0900

    Initialize project

---
+++ client-server-1436/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  client-server-1436
+//
+//  Created by Василий Слепцов on 09.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
